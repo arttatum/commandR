1c675fc12459632adf1c0f0103043fbc7553fb61
will <william.loughney@thoughtworks.com>
2021-12-24 10:09:29 +0000

    Added test of concurrent execution.

--- lib/src/test/java/commandR/implementations/MediatorTest.java
+++ lib/src/test/java/commandR/implementations/MediatorTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +21,7 @@ class MediatorTest extends TestData {
         ArrayList<RequestHandler<?,?>> list = new ArrayList<>(){};
         list.add(new CapitalizeHandler());
         list.add(new RemoveVowelsHandler());
+        list.add(new IncrementCounterHandler());
         Registry registry = new commandR.implementations.Registry().withHandlers(list);
         mediator = new commandR.implementations.Mediator(registry);
     }
@@ -29,5 +33,28 @@ class MediatorTest extends TestData {
 
         String removeVowelsResponse = mediator.send(new RemoveVowels("Hello"));
         assertEquals(removeVowelsResponse, "Hll");
+    }
+
+    @Test
+    void send_concurrent_requests_handled_in_parallel() throws InterruptedException {
+        int numberOfThreads = 1000;
+        int lengthOfPauseInMs = 100;
+
+        ExecutorService pool = Executors.newFixedThreadPool(numberOfThreads);
+        CountDownLatch latch = new CountDownLatch(numberOfThreads);
+        MyCounter counter = new MyCounter();
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < numberOfThreads; i++) {
+            pool.execute(() -> {
+                mediator.send(new IncrementCounter(counter, lengthOfPauseInMs));
+                latch.countDown();
+            });
+        }
+        latch.await();
+        long finish = System.currentTimeMillis();
+        long timeElapsed = finish - start;
+
+        assertEquals (numberOfThreads, counter.getCount());
+        assert (lengthOfPauseInMs * numberOfThreads > timeElapsed);
     }
 }
--- lib/src/test/java/commandR/implementations/TestData.java
+++ lib/src/test/java/commandR/implementations/TestData.java
@@ -3,10 +3,11 @@ package commandR.implementations;
 import commandR.interfaces.Request;
 import commandR.interfaces.RequestHandler;
 
+import javax.lang.model.type.NullType;
 import java.util.Locale;
 
 public class TestData {
-    protected static class Capitalize implements Request<String> {
+    protected static class Capitalize implements Request<String>{
         private final String message;
 
         public Capitalize(String message) {
@@ -33,6 +34,41 @@ public class TestData {
         @Override
         public String handle(RemoveVowels request) {
             return request.message.replaceAll("[aeiouAEIOU]", "");
+        }
+    }
+
+    protected class IncrementCounter implements Request<NullType> {
+        private final MyCounter counter;
+        private final int lengthOfPauseInMs;
+
+        public IncrementCounter(MyCounter counter, int lengthOfPauseInMs) {
+            this.counter = counter;
+            this.lengthOfPauseInMs = lengthOfPauseInMs;
+        }
+    }
+
+    protected class IncrementCounterHandler implements RequestHandler<IncrementCounter, NullType> {
+        @Override
+        public NullType handle(IncrementCounter request) {
+            try {
+                request.counter.increment();
+                Thread.sleep(request.lengthOfPauseInMs);
+            } catch (InterruptedException ex) {
+                System.err.println(ex);
+            }
+            return null;
+        }
+    }
+
+    protected static class MyCounter {
+        private int count;
+        protected synchronized void increment() throws InterruptedException {
+            int temp = count;
+            count = temp + 1;
+        }
+
+        protected int getCount() {
+            return count;
         }
     }
 }
